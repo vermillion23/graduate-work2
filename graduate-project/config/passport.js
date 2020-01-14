223e13301dc45aee00916f1ffd387358a6933631
vermillion23 <den4ik.frolov@gmail.com>
2020-01-14 17:33:28 +0300

    Autorization feature

---
+++ graduate-project/config/passport.js
@@ -0,0 +1,38 @@
+
+const mongoose = require('mongoose');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+
+const Users = mongoose.model('Users');
+
+passport.use(new LocalStrategy({
+    usernameField: 'user[email]',
+    passwordField: 'user[password]',
+}, (email, password, done) => {
+    Users.findOne({ email })
+        .then((user) => {
+            if (!user || !user.validatePassword(password)) {
+                return done(null, false, { errors: { 'email or password': 'is invalid' } });
+            }
+
+            return done(null, user);
+        }).catch(done);
+}));
+
+//app.post('/user', jsonParser, function (req, res) {
+//    var user = req.body;
+
+//    var savedata = new Model({
+
+//        'userName': user.userName,
+//        'userPassword': user.userPassword,
+//        'userEmail': user.userPassword
+
+//    }).save(function (err, result) {
+//        if (err) res.status(500);
+
+//        if (result) {
+//            res.json(result);
+//        }
+//    })
+//})
---
+++ graduate-project/public/script.js
@@ -0,0 +1,29 @@
+// async function send () {
+//     event.preventDefault();
+// Функция должна записывать данные ввода в базу данных в формате JSON? 
+// и редиректить на главную
+// должна включать проверки данных на клиенте
+//     let result = await fetch("server.js"), { method: "POST", body: new FormData(document.forms[0]) });
+
+// };
+
+document.getElementById("submit").addEventListener("click", function (e) {
+    e.preventDefault();
+    // получаем данные формы
+    let registerForm = document.forms["registerForm"];
+    let userName = registerForm.elements["userName"].value;
+    let userPassword = registerForm.elements["userPassword"].value;
+    let userEmail = registerForm.elements["userEmail"].value;
+    // сериализуем данные в json
+    let user = JSON.stringify({ userName: userName, userPassword: userPassword, userEmail: userEmail });
+    let request = new XMLHttpRequest();
+    // посылаем запрос на адрес "/user"
+    request.open("POST", "http://127.0.0.1:1337/user", true);
+    request.setRequestHeader("Content-Type", "application/json");
+    request.addEventListener("load", function () {
+        // получаем и парсим ответ сервера
+        let receivedUser = JSON.parse(request.response);
+        console.log(receivedUser.userName, "-", receivedUser.userEmail);   // смотрим ответ сервера
+    });
+    request.send(user);
+});
